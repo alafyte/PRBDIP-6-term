08bfbca52e51189fb51aa38f05e388c864130065
alafyte <xleb.xlebnyj@mail.ru>
2024-03-09 13:34:16 +0300

    Лабораторная работа №4

--- Lab04/MSSQL/Lab04.sql
+++ Lab04/MSSQL/Lab04.sql
@@ -1,26 +1,26 @@
 -- 6.	Определите тип пространственных данных во всех таблицах.
 SELECT DISTINCT SUBSTRING(geom.STAsText(), 1, CHARINDEX(' ', geom.STAsText() + ' ') - 1) as GeometryType
-FROM NE_110M_LAND;
+FROM world_countries;
 
 -- 7.	Определите SRID.
 SELECT DISTINCT GEOM.STSrid AS SRID
-FROM NE_110M_LAND;
+FROM world_countries;
 
 -- 8.	Определите атрибутивные столбцы.
 SELECT COLUMN_NAME
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'NE_110M_LAND';
+WHERE TABLE_NAME = 'world_countries';
 
 -- 9.	Верните описания пространственных объектов в формате WKT.
 SELECT geom.STAsText() AS wkt_description
-FROM NE_110M_LAND;
+FROM world_countries;
 
 -- 10.1. Нахождение пересечения пространственных объектов
 SELECT A.geom.STIntersection(B.geom) AS intersection_geom
 FROM (SELECT id, geom.MakeValid() AS geom
-      FROM ne_110m_land) A,
+      FROM world_countries) A,
      (SELECT id, geom.MakeValid() AS geom
-      FROM ne_110m_land) B
+      FROM world_countries) B
 WHERE A.id <> B.id
   AND A.geom.STIntersects(B.geom) = 1;
 
@@ -28,13 +28,13 @@ WHERE A.id <> B.id
 -- 10.2. Нахождение координат вершин пространственного объекта
 DECLARE @geom geometry, @id int = 49;
 SET @geom = (SELECT geom.STAsText() AS vertice
-             FROM ne_110m_land
+             FROM world_countries
              WHERE id = @id);
 SELECT @geom.STAsText() AS vertices;
 
 -- 10.3. Нахождение площади пространственных объектов
 SELECT id, geom.MakeValid().STArea() AS area
-FROM ne_110m_land;
+FROM world_countries;
 
 
 -- 11.	Создайте пространственный объект в виде точки (1) /линии (2) /полигона (3).
@@ -42,29 +42,37 @@ FROM ne_110m_land;
 DECLARE @point geometry;
 DECLARE @line geometry;
 DECLARE @polygon geometry;
-DECLARE @rel_line NVARCHAR(100);
-DECLARE @rel_polygon NVARCHAR(100);
-DECLARE @rel_line_polygon NVARCHAR(100);
 
-SET @point = geometry::STPointFromText('POINT(15 10)', 0);
-SET @line = geometry::STLineFromText('LINESTRING(0 0, 10 10, 20 25, 50 60)', 0);
-SET @polygon = geometry::STPolyFromText('POLYGON((0 0, 20 0, 20 30, 0 30, 0 0))', 0);
+SET @point = geometry::STPointFromText('POINT(15 10)', 4326);
+SET @line = geometry::STLineFromText('LINESTRING(0 0, 10 10, 20 25, 50 60)', 4326);
+SET @polygon = geometry::STPolyFromText('POLYGON((0 0, 20 0, 20 30, 0 30, 0 0))', 4326);
 
-SET @rel_line = CASE WHEN @point.STIntersects(@line) = 1 THEN 'Intersects' ELSE 'Does not intersect' END;
-SET @rel_polygon = CASE WHEN @point.STIntersects(@polygon) = 1 THEN 'Intersects' ELSE 'Does not intersect' END;
-SET @rel_line_polygon = CASE WHEN @line.STIntersects(@polygon) = 1 THEN 'Intersects' ELSE 'Does not intersect' END;
+SELECT @point;
+SELECT @line ;
+SELECT @polygon;
 
-PRINT 'Point Intersects Line: ' + @rel_line;
-PRINT 'Point Intersects Polygon: ' + @rel_polygon;
-PRINT 'Line Intersects Polygon: ' + @rel_line_polygon;
+SELECT country FROM world_countries WHERE geom.STIntersects(@point) = 1;
+SELECT country FROM world_countries WHERE geom.STIntersects(@line) = 1;
+SELECT country FROM world_countries WHERE geom.STIntersects(@polygon) = 1;
+
 
 -- 13.	Продемонстрируйте индексирование пространственных объектов.
 CREATE SPATIAL INDEX spatial_index_land
-    ON ne_110m_land (geom)
+    ON world_countries (geom)
     USING GEOMETRY_GRID
     WITH (
     BOUNDING_BOX = (-180, -189, 180, 180)
     );
+
+--DROP INDEX spatial_index_land ON world_countries
+
+DECLARE @rect geometry;
+SET @rect = geometry::STGeomFromText('POLYGON((-10 -10, 10 -10, 10 10, -10 10, -10 -10))', 4326);
+SELECT @rect;
+SELECT country, geom
+FROM world_countries
+WHERE geom.STIntersects(@rect) = 1;
+
 
 -- 14. Разработайте хранимую процедуру, которая принимает координаты точки и возвращает пространственный объект,
 -- в который эта точка попадает.
@@ -80,7 +88,7 @@ BEGIN
                                       4326);
 
     SELECT TOP 1 @p_Polygon = ID
-    FROM ne_110m_land
+    FROM world_countries
     WHERE geom.MakeValid().STIntersects(@v_Point) = 1;
 END;
 
@@ -89,6 +97,6 @@ GO
 BEGIN
     DECLARE @v_polygon INT;
     EXEC GetPolygonFromPoint 30, 40, @v_polygon OUTPUT;
-    PRINT 'Polygon: ' + CAST(@v_polygon AS VARCHAR)
+    SELECT geom FROM world_countries where id = @v_polygon;
 END
 
--- Lab04/Oracle/Lab04.sql
+++ Lab04/Oracle/Lab04.sql
@@ -1,83 +1,95 @@
 -- 6.	Определите тип пространственных данных во всех таблицах.
-SELECT CASE
-           SDO_GEOMETRY.GET_GTYPE(GEOM)
-           WHEN 0 THEN 'UNKNOWN_GEOMETRY'
-           WHEN 1 THEN 'POINT'
-           WHEN 2 THEN 'LINE or CURVE'
-           WHEN 3 THEN 'POLYGON or SURFACE'
-           WHEN 4 THEN 'COLLECTION'
-           WHEN 5 THEN 'MULTIPOINT'
-           WHEN 6 THEN 'MULTILINE or MULTICURVE'
-           WHEN 7 THEN 'MULTIPOLYGON or MULTISURFACE'
-           WHEN 8 THEN 'SOLID'
-           WHEN 9 THEN 'MULTISOLID'
-           ELSE 'UNKNOWN'
-           END AS geometry_type
-FROM NE_110M_LAND;
+SELECT DISTINCT CASE
+                    SDO_GEOMETRY.GET_GTYPE(GEOM)
+                    WHEN 0 THEN 'UNKNOWN_GEOMETRY'
+                    WHEN 1 THEN 'POINT'
+                    WHEN 2 THEN 'LINE or CURVE'
+                    WHEN 3 THEN 'POLYGON or SURFACE'
+                    WHEN 4 THEN 'COLLECTION'
+                    WHEN 5 THEN 'MULTIPOINT'
+                    WHEN 6 THEN 'MULTILINE or MULTICURVE'
+                    WHEN 7 THEN 'MULTIPOLYGON or MULTISURFACE'
+                    WHEN 8 THEN 'SOLID'
+                    WHEN 9 THEN 'MULTISOLID'
+                    ELSE 'UNKNOWN'
+                    END AS geometry_type
+FROM WORLD_COUNTRIES;
 
 
 -- 7.	Определите SRID.
-SELECT COLUMN_NAME,
-       SRID
+SELECT SRID
 FROM USER_SDO_GEOM_METADATA
-WHERE TABLE_NAME = 'NE_110M_LAND';
+WHERE TABLE_NAME = 'WORLD_COUNTRIES';
 
 -- 8.	Определите атрибутивные столбцы.
 SELECT COLUMN_NAME
 FROM USER_TAB_COLUMNS
-WHERE TABLE_NAME = 'NE_110M_LAND';
+WHERE TABLE_NAME = 'WORLD_COUNTRIES';
 
 -- 9.	Верните описания пространственных объектов в формате WKT.
 SELECT SDO_UTIL.TO_WKTGEOMETRY(geom) AS wkt_description
-FROM NE_110M_LAND;
+FROM WORLD_COUNTRIES;
 
 -- 10.1. Нахождение пересечения пространственных объектов
-SELECT SDO_GEOM.SDO_INTERSECTION(a.geom, b.geom, 0.005) AS intersection_geom
-FROM NE_110M_LAND a,
-     NE_110M_LAND b
+SELECT a."country"
+FROM WORLD_COUNTRIES a,
+     WORLD_COUNTRIES b
 WHERE a.id <> b.id
   AND SDO_RELATE(a.geom, b.geom, 'mask=anyinteract') = 'TRUE';
 
+
 -- 10.2. Нахождение координат вершин пространственного объекта
 SELECT SDO_UTIL.TO_WKTGEOMETRY(geom) AS vertices
-FROM NE_110M_LAND;
+FROM WORLD_COUNTRIES;
 
 -- 10.3. Нахождение площади пространственных объектов
 SELECT SDO_GEOM.SDO_AREA(geom, 0.005) AS area
-FROM NE_110M_LAND;
+FROM WORLD_COUNTRIES;
 
 -- 11.	Создайте пространственный объект в виде точки (1) /линии (2) /полигона (3).
 -- 12.	Найдите, в какие пространственные объекты попадают созданные вами объекты.
+SELECT SDO_GEOMETRY(2002, 4326, NULL, SDO_ELEM_INFO_ARRAY(1, 2, 1),
+                    SDO_ORDINATE_ARRAY(0, 0, 10, 10, 20, 25, 50, 60))
+FROM DUAL;
+SELECT SDO_GEOMETRY(2001, 4326, SDO_POINT_TYPE(15, 10, NULL), NULL, NULL)
+FROM DUAL;
+SELECT SDO_GEOMETRY(2003, 4326, NULL, SDO_ELEM_INFO_ARRAY(1, 1003, 1),
+                    SDO_ORDINATE_ARRAY(0, 0, 20, 0, 20, 30, 0, 30, 0, 0))
+FROM DUAL;
 
 DECLARE
-    point       SDO_GEOMETRY;
-    line        SDO_GEOMETRY;
-    polygon     SDO_GEOMETRY;
-    rel_line    VARCHAR2(100);
-    rel_polygon VARCHAR2(100);
-    rel_line_polygon VARCHAR2(100);
+    point   SDO_GEOMETRY;
+    line    SDO_GEOMETRY;
+    polygon SDO_GEOMETRY;
 BEGIN
-    point := SDO_GEOMETRY(2001, NULL, SDO_POINT_TYPE(15, 10, NULL), NULL, NULL);
-    line := SDO_GEOMETRY(2002, NULL, NULL, SDO_ELEM_INFO_ARRAY(1, 2, 1),
+    point := SDO_GEOMETRY(2001, 4326, SDO_POINT_TYPE(15, 10, NULL), NULL, NULL);
+    line := SDO_GEOMETRY(2002, 4326, NULL, SDO_ELEM_INFO_ARRAY(1, 2, 1),
                          SDO_ORDINATE_ARRAY(0, 0, 10, 10, 20, 25, 50, 60));
-    polygon := SDO_GEOMETRY(2003, NULL, NULL, SDO_ELEM_INFO_ARRAY(1, 1003, 1),
+    polygon := SDO_GEOMETRY(2003, 4326, NULL, SDO_ELEM_INFO_ARRAY(1, 1003, 1),
                             SDO_ORDINATE_ARRAY(0, 0, 20, 0, 20, 30, 0, 30, 0, 0));
-
-    SELECT SDO_RELATE(point, line, 'mask=anyinteract') INTO rel_line FROM DUAL;
-    SELECT SDO_RELATE(point, polygon, 'mask=anyinteract') INTO rel_polygon FROM DUAL;
-    SELECT SDO_RELATE(line, polygon, 'mask=anyinteract') INTO rel_line_polygon FROM DUAL;
-
-    DBMS_OUTPUT.PUT_LINE('Point Intersects Line: ' || rel_line);
-    DBMS_OUTPUT.PUT_LINE('Point Intersects Polygon: ' || rel_polygon);
-    DBMS_OUTPUT.PUT_LINE('Line Intersects Polygon: ' || rel_line_polygon);
+    FOR country_rec IN (SELECT * FROM WORLD_COUNTRIES WHERE SDO_RELATE(GEOM, point, 'mask=anyinteract') = 'TRUE')
+        LOOP
+            DBMS_OUTPUT.PUT_LINE('Country: ' || country_rec."country");
+        END LOOP;
+    FOR country_rec IN (SELECT * FROM WORLD_COUNTRIES WHERE SDO_RELATE(GEOM, line, 'mask=anyinteract') = 'TRUE')
+        LOOP
+            DBMS_OUTPUT.PUT_LINE('Countries by line : ' || country_rec."country");
+        END LOOP;
+    FOR country_rec IN (SELECT * FROM WORLD_COUNTRIES WHERE SDO_RELATE(GEOM, polygon, 'mask=anyinteract') = 'TRUE')
+        LOOP
+            DBMS_OUTPUT.PUT_LINE('Countries by polygon: ' || country_rec."country");
+        END LOOP;
 END;
 
 -- 13.	Продемонстрируйте индексирование пространственных объектов.
 -- drop index spatial_index_land;
 CREATE INDEX spatial_index_land
-ON NE_110M_LAND(GEOM)
-INDEXTYPE IS MDSYS.SPATIAL_INDEX
-PARAMETERS('layer_gtype=MULTIPOLYGON');
+    ON WORLD_COUNTRIES (GEOM)
+    INDEXTYPE IS MDSYS.SPATIAL_INDEX
+    PARAMETERS ('layer_gtype=MULTIPOLYGON');
+
+SELECT "country" FROM world_countries WHERE SDO_RELATE(geom, SDO_GEOMETRY(2003, 4326, NULL, SDO_ELEM_INFO_ARRAY(1, 1003, 1),
+                    SDO_ORDINATE_ARRAY(-10, -10, 10, -10, 10, 10, -10, 10, -10, -10)), 'mask=anyinteract') = 'TRUE';
 
 
 -- 14. Разработайте хранимую процедуру, которая принимает координаты точки и возвращает пространственный объект,
@@ -92,22 +104,26 @@ AS
 BEGIN
     DECLARE
         v_Point SDO_GEOMETRY := SDO_GEOMETRY(
-            2001,
-            4326,
-            SDO_POINT_TYPE(p_X, p_Y, NULL),
-            NULL,
-            NULL
-        );
+                2001,
+                4326,
+                SDO_POINT_TYPE(p_X, p_Y, NULL),
+                NULL,
+                NULL
+            );
     BEGIN
-        SELECT ID INTO p_Polygon
-        FROM ne_110m_land
-        WHERE SDO_RELATE(geom, v_Point, 'mask=contains') = 'TRUE' AND ROWNUM = 1;
+        SELECT id
+        INTO p_Polygon
+        FROM WORLD_COUNTRIES
+        WHERE SDO_RELATE(geom, v_Point, 'mask=contains') = 'TRUE'
+          AND ROWNUM = 1;
     END;
 END;
 
 DECLARE
     v_ResultPolygon INT;
+    v_ResultCountry varchar2(2047);
 BEGIN
     GetPolygonFromPoint(30, 40, v_ResultPolygon);
-    DBMS_OUTPUT.PUT_LINE('Polygon: ' || v_ResultPolygon);
+    SELECT "country" INTO v_ResultCountry FROM WORLD_COUNTRIES WHERE id = 362;
+    DBMS_OUTPUT.PUT_LINE('Country: ' || v_ResultCountry);
 END;
